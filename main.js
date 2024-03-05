f88becada2847f8b0b11592e73707cb45fe80c18
Munawwar khan <92468595+Munawwar0@users.noreply.github.com>
2024-03-05 17:40:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ let loginForm = document.querySelector('.login-form-container');
 let formClose= document.querySelector('#form-close');
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let videoBtn =document.querySelectorAll('.vid-btn');
 
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
 }
-navbar.addEventListener('click', () => {
-    navbar.classList.toggle('fa-times');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-});
+})
 searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
@@ -23,5 +28,14 @@ formBtn.addEventListener('click', () =>{
 });
 
 formClose.addEventListener('click', ()=>{
-    loginForm.classList.remove('active')
-})
+    loginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click',() =>{
+        document.querySelector('.control .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-solid').src =src;
+    });
+});
